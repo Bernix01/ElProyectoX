2b6519b5077f932e4a8793bf35d16a0a60be0e43
CarlosCedeniio <caencede@espol.edu.ec>
2015-12-13 13:21:18 -0500

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @CarlosCedeniio
+ */
+public class Usuario {
+    private int id;
+    public Usuario(){
+        this.id=0;
+    }
+    public Usuario(int id){
+        this.id=id;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+}

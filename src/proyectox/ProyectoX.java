5c870a4134abc3a534c9fb72b67824b95990c77a
bryan12ord <bryan12ordoez@hotmail.es>
2015-12-13 19:22:09 -0500

    Update ProyectoX.java

--- src/proyectox/ProyectoX.java
+++ src/proyectox/ProyectoX.java
@@ -1,5 +1,34 @@
 package proyectox;
 
 public class ProyectoX {
+  public LinkedList<Pelicula> Top10Categoria (LinkedList<> P,String Categoria){
+    LinkedList<> C = New Linked List<>;
+    Iterator <> iter = P.iterator();
+    while (P.hasNext()){
+      Pelicula T = (Pelicula) P.next();
+      if (T.categoria == this.Categoria)
+        C.add(T);
+    }
+    Collections.sort(C, new Comparator() {
+	  @Override
+	  public int compare(Pelicula p1, Pelicula p2) {
+	  	return new Integer(p2.getRating()).compareTo(new Integer(p1.getRating()));
+	  }
+      }
+  
+  public LinkedList<Calificacion> CalificacionesFecha (LinkedList<> C){
+    Collection.sort(C, new Comparator(){
+      @override
+      public int compare(Calificacion c1, Calificacion c2){
+        return c1.getDateTime().compareTo(c2.getDateTime());
+        
+      }
+    }
+  }
+    
+    
+  
+  }
+  
 
 }
